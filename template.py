7ae2d13704f491c93c193daf2ce09264841eabfd
Akash-Vijay <akashvijay789@gmail.com>
2024-04-17 20:20:08 +0530

    [added] folder structure for project

--- template.py
+++ template.py
@@ -8,7 +8,6 @@ list_of_file = [
     "src/__init__.py",
     "src/helper.py",
     "src/prompt.py",
-    ".env",
     "setup.py",
     "research/experiments.ipynb",
     "app.py",
@@ -18,17 +17,17 @@ list_of_file = [
 ]
 
 for filepath in list_of_file:
-    Path(filepath).touch()
+    filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
 
-    if filedir !="":
-      os.makedirs(filedir, exist_ok=True)
-      logging.info(f"Creating directory; {filedir} for the file {filename}")
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file {filename}")
 
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
-       with open(filepath, 'w') as f:
-          pass
-          logging.info(f"Creating empty file: {filepath}")
+        with open(filepath, 'w') as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
 
     else:
-       logging.info(f"{filename} is already created")
+        logging.info(f"{filename} is already created")
