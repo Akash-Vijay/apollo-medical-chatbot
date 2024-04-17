fc87a85c2997c703a79060cd93bd8c6d613f0ba0
Akash-Vijay <akashvijay789@gmail.com>
2024-04-17 20:23:27 +0530

    [added] setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Apollo Medical Chatbot',
+    version='0.0.1',
+    author='Akash Vijay',
+    author_email='akashvijay789@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+
+)
